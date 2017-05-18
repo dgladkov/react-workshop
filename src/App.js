5b5cad6fded16d8290e2e9a957580d40028425f1
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2017-05-18 17:09:47 +0300

    next step

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Hello world',
+      todos: [
+        { text: 'Buy milk', complete: true },
+        { text: 'Buy milk', complete: true },
+        { text: 'Buy milk', complete: false },
+      ],
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="todo-app">
+        <h2>Thursday tasks</h2>
+        <ul>
+          <li>
+            <p className="checked">Buy milk</p>
+            <button>
+              Delete item
+            </button>
+          </li>
+          <li>
+            <p>Pay bills</p>
+            <button>
+              Delete item
+            </button>
+          </li>
+        </ul>
+        <input placeholder="New todo" />
+        <button>Add item</button>
       </div>
     );
   }
