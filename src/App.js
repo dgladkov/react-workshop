4d4989d7eb754a428315bb5c284c1253ad4526b2
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2017-05-25 15:24:44 +0300

    session one

--- src/App.js
+++ src/App.js
@@ -1,37 +1,81 @@
 import React, { Component } from 'react';
 
+let currentIndex = 0;
+function getNextIndex() {
+  return ++currentIndex;
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: 'Hello world',
-      todos: [
-        { text: 'Buy milk', complete: true },
-        { text: 'Buy milk', complete: true },
-        { text: 'Buy milk', complete: false },
-      ],
+      todos: [],
+      newTodoText: '',
     };
+    this.changeNewTodoText = this.changeNewTodoText.bind(this);
+    this.createNewTodo = this.createNewTodo.bind(this);
   }
+
+  changeNewTodoText(e) {
+    this.setState({
+      newTodoText: e.target.value,
+    });
+  }
+
+  createNewTodo() {
+    this.setState({
+      todos: [...this.state.todos, {
+        id: getNextIndex(),
+        text: this.state.newTodoText,
+        complete: false,
+      }],
+      newTodoText: '',
+    });
+  }
+
+  toggleTodoItem(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (id === todo.id) {
+          return {...todo, complete: !todo.complete};
+        }
+        return todo;
+      }),
+    });
+  }
+
+  deleteTodoItem(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id),
+    });
+  }
+
   render() {
     return (
       <div id="todo-app">
         <h2>Thursday tasks</h2>
         <ul>
-          <li>
-            <p className="checked">Buy milk</p>
-            <button>
-              Delete item
-            </button>
-          </li>
-          <li>
-            <p>Pay bills</p>
-            <button>
-              Delete item
-            </button>
-          </li>
+          {this.state.todos.map((todo) => (
+            <li key={todo.id}>
+              <p
+                onClick={() => this.toggleTodoItem(todo.id)}
+                className={todo.complete ? 'checked' : null}
+              >
+                {todo.text}
+              </p>
+              <button onClick={() => this.deleteTodoItem(todo.id)}>
+                Delete item
+              </button>
+            </li>
+          ))}
         </ul>
-        <input placeholder="New todo" />
-        <button>Add item</button>
+        <input
+          placeholder="New todo"
+          onChange={this.changeNewTodoText}
+          value={this.state.newTodoText}
+        />
+        <button onClick={this.createNewTodo}>Add item</button>
       </div>
     );
   }
